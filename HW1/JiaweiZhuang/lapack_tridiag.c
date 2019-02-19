930080a63be7505fa0ee38770dbae0c3fadbc6ef
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2019-02-19 10:29:58 -0500

    HW1P4 tri-diagonal solver code

---
+++ HW1/JiaweiZhuang/lapack_tridiag.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <lapacke.h>
+
+#define N 5
+
+int main(int argc, char* argv[])
+{
+    // Initialize array
+    // TODO: use input data
+    double a[N-1] = {-1, -1, -1, -1}; // lower diagonal
+    double b[N] = {2, 2, 2, 2, 2}; // major diagonal
+    double c[N-1] = {-1, -1, -1, -1}; // upper diagonal
+
+    double f[N] = {1, 2, 3, 4, 5}; // right hand side, will be overwritten by solution
+
+    // Solve linear system with LAPACK
+    lapack_int info,n,nrhs,ldb;
+    n = N;
+    nrhs = 1;  // only a single column on RHS
+    ldb = N;
+
+    info = LAPACKE_dgtsv(LAPACK_COL_MAJOR,n,nrhs,a,b,c,f,ldb);
+
+    // print result to check
+    for(int i = 0; i < N; i++) {
+            printf("%f ", f[i]);
+        }
+    printf("\n");
+
+}

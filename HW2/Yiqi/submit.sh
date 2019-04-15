8e5075c6d951ed1b10946fb275e550fee38ef705
Yiqi Xie <yiqi_xie@g.harvard.edu>
2019-04-15 16:34:05 -0400

    updated P2 solution

--- HW2/Yiqi/submit.sh
+++ HW2/Yiqi/submit.sh
@@ -7,11 +7,15 @@
 #SBATCH --gres=gpu:1
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 0-00:01
-#SBATCH -o job_%j.out
-#SBATCH -e job_%j.err
+#SBATCH -t 0-00:05
+##SBATCH -o job_%j.out
+##SBATCH -e job_%j.err
 
-# Can run commands here, or use ‘echo’ to look at CUDA_VISIBLE_DEVICES
 hostname
 echo $CUDA_VISIBLE_DEVICES
-time ./parallel 8
+{ time ./parallel 4; } >trial_4.out 2>&1
+{ time ./parallel 8; } >trial_8.out 2>&1
+{ time ./parallel 16; } >trial_16.out 2>&1
+{ time ./parallel 32; } >trial_32.out 2>&1
+{ time ./parallel 64; } >trial_64.out 2>&1
+{ time ./parallel 128; } >trial_128.out 2>&1
